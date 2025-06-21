5d636f44d4f37d900288f70bb9eac8e38102a382
Gabriel Tavares <gabrieltavares.crvg@gmail.com>
2025-06-20 22:27:08 -0300

    adicionando mais validações a pagina entrar

--- paginas/login/entrar.js
+++ paginas/login/entrar.js
@@ -1,13 +1,22 @@
 //validação usada somente para portfolio e não para uma aplicação real
-let emailUsuario = 'teste@teste.com';
-let senhaUsuario = '12345678';
+const emailUsuario = 'teste@teste.com';
+const senhaUsuario = '12345678';
 
 function fazerLogin() {
     const alerta = document.getElementById('alerta')
+    const email = document.getElementById('email').value
+    const senha = document.getElementById('senha').value;
+
 
     event.preventDefault()
 
-    if (validarEmail() && validarSenha()) {
+    if (email === "" && senha === "") {
+        alerta.textContent = "Preencha os campos"
+    } else if (email === "") {
+        alerta.textContent = "Preencha o campo email"
+    } else if (senha === "") {
+        alerta.textContent = "Preencha o campo senha"
+    } else if (validarEmail() && validarSenha()) {
         window.location.href = "../../paginas/home/home.html";
     } else {
         alerta.textContent = "Email ou senha inválidos.";
